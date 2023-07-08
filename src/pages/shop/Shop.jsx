3400d54bd1d904094075d905c1b66e7c120e2c6f
unknown <smitpatel40014@gmail.com>
2023-07-08 04:22:43 -0700

    first commit

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { PRODUCTS } from '../../products'
+import Product from './Product'
+import './shop.css';
+
+const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>SmitPatel Shop</h1>
+        </div>
+        <div className="products">
+            {PRODUCTS.map((product)=>(<Product data={product}/>)
+            )}
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Shop
